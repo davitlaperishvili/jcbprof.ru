4a8990e66112701c2729f2c274d7d63f81c208c7
dlaperishvili <davitlaperishvili@gmail.com>
2023-12-01 16:25:53 +0400

    home page progress

--- wp-content/themes/dltheme/index.php
+++ wp-content/themes/dltheme/index.php
@@ -6,6 +6,270 @@
   <title>jcbprof.ru Test TEMPLATE</title>
 </head>
 <body>
-  
+
+<main class="dl_theme page_wrapper">
+  <section class="hero">
+    <div class="container">
+      <div class="hero_grid">
+        <div class="hero_banner left">
+          <figure>
+            <img src="catalog/view/theme/Gobazar/image/images/hero1.jpg" alt="">
+          </figure>
+          <div class="banner_content">
+            <div class="text"><span>13 дней</span> осталось</div>
+            <div class="banner_button text">
+              <a href="#">Перейти в каталог</a>
+            </div>
+          </div>
+        </div>
+        <div class="hero_banners_slider_wrap">
+          <div class="hero_banners_slider owl-carousel">
+            <div class="hero_slider_item">
+              <figure>
+                <img src="catalog/view/theme/Gobazar/image/images/hero_slide1.jpg" alt="">
+              </figure>
+              <div class="banner_content">
+                <div class="banner_title">КАЧЕСТВО <br> <span>ПО ЛУЧШЕЙ ЦЕНЕ</span> </div>
+                <div class="banner_subtitle">Более 2500 товаров</div>
+                <div class="banner_points">
+                  <ul>
+                    <li>
+                      <img src="catalog/view/theme/Gobazar/image/images/point1.svg" alt="">
+                      <span>Заказы 24/7</span>
+                    </li>
+                    <li>
+                      <img src="catalog/view/theme/Gobazar/image/images/point2.svg" alt="">
+                      <span>Помощь в подборе</span>
+                    </li>
+                    <li>
+                      <img src="catalog/view/theme/Gobazar/image/images/point3.svg" alt="">
+                      <span>Быстрая отгрузка</span>
+                    </li>
+                    <li>
+                      <img src="catalog/view/theme/Gobazar/image/images/point4.svg" alt="">
+                      <span>Гарантия</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="hero_slider_item">
+              <figure>
+                <img src="catalog/view/theme/Gobazar/image/images/hero_slide1.jpg" alt="">
+              </figure>
+              <div class="banner_content">
+                <div class="banner_title">КАЧЕСТВО <br> <span>ПО ЛУЧШЕЙ ЦЕНЕ</span> </div>
+                <div class="banner_subtitle">Более 2500 товаров</div>
+                <div class="banner_points">
+                  <ul>
+                    <li>
+                      <img src="catalog/view/theme/Gobazar/image/images/point1.svg" alt="">
+                      <span>Заказы 24/7</span>
+                    </li>
+                    <li>
+                      <img src="catalog/view/theme/Gobazar/image/images/point2.svg" alt="">
+                      <span>Помощь в подборе</span>
+                    </li>
+                    <li>
+                      <img src="catalog/view/theme/Gobazar/image/images/point3.svg" alt="">
+                      <span>Быстрая отгрузка</span>
+                    </li>
+                    <li>
+                      <img src="catalog/view/theme/Gobazar/image/images/point4.svg" alt="">
+                      <span>Гарантия</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="hero_slider_item">
+              <figure>
+                <img src="catalog/view/theme/Gobazar/image/images/hero_slide1.jpg" alt="">
+              </figure>
+              <div class="banner_content">
+                <div class="banner_title">КАЧЕСТВО <br> <span>ПО ЛУЧШЕЙ ЦЕНЕ</span> </div>
+                <div class="banner_subtitle">Более 2500 товаров</div>
+                <div class="banner_points">
+                  <ul>
+                    <li>
+                      <img src="catalog/view/theme/Gobazar/image/images/point1.svg" alt="">
+                      <span>Заказы 24/7</span>
+                    </li>
+                    <li>
+                      <img src="catalog/view/theme/Gobazar/image/images/point2.svg" alt="">
+                      <span>Помощь в подборе</span>
+                    </li>
+                    <li>
+                      <img src="catalog/view/theme/Gobazar/image/images/point3.svg" alt="">
+                      <span>Быстрая отгрузка</span>
+                    </li>
+                    <li>
+                      <img src="catalog/view/theme/Gobazar/image/images/point4.svg" alt="">
+                      <span>Гарантия</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="hero_banner right">
+          <figure>
+            <img src="catalog/view/theme/Gobazar/image/images/hero1.jpg" alt="">
+          </figure>
+          <div class="banner_content">
+            <div class="text title">Новые товары в распродаже</div>
+            <div class="banner_button">
+              <a href="#">Перейти в каталог</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="benefits">
+    <div class="container">
+      <div class="section_title">Наши преимущества</div>
+      <div class="block_wrap">
+        <div class="benefits_list">
+          <div class="benefit_item">
+            <figure>
+              <img src="catalog/view/theme/Gobazar/image/images/benefit1.svg" alt="">
+            </figure>
+            <div class="benefit_content">
+              <div class="benefit_title">ЗАПЧАСТИ С ДОСТАВКОЙ!</div>
+              <div class="benefit_text">Доставка до ТК – БЕСПЛАТНО. <br> Стоимость доставки до точки - индивидуально.</div>
+            </div>
+          </div>
+          <div class="benefit_item">
+            <figure>
+              <img src="catalog/view/theme/Gobazar/image/images/benefit2.svg" alt="">
+            </figure>
+            <div class="benefit_content">
+              <div class="benefit_title">ЗАПЧАСТИ С ЗАВОДОВ НАПРЯМУЮ!</div>
+              <div class="benefit_text">Наши партнеры - производители запчастей на JCB, Komatsu, Volvo, CAT и другие</div>
+            </div>
+          </div>
+          <div class="benefit_item">
+            <figure>
+              <img src="catalog/view/theme/Gobazar/image/images/benefit3.svg" alt="">
+            </figure>
+            <div class="benefit_content">
+              <div class="benefit_title">ЗАПЧАСТИ JCB НА 3CX И 4CX!</div>
+              <div class="benefit_text">Огромный каталог запчастей на самые популярные экскаваторы-погрузчики!</div>
+            </div>
+          </div>
+          <div class="benefit_item">
+            <figure>
+              <img src="catalog/view/theme/Gobazar/image/images/benefit1.svg" alt="">
+            </figure>
+            <div class="benefit_content">
+              <div class="benefit_title">ЗАПЧАСТИ ОПТОМ!</div>
+              <div class="benefit_text">Предлагаем выгодные оптовые цены поставщикам запчастей на спецтехнику в России!</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="categories_list">
+    <div class="container">
+      <div class="section_title">Каталог запчастей для спецтехники</div>
+      <div class="categories_catalog">
+        <div class="catalog_item"></div>
+      </div>
+    </div>
+  </section>
+  <section class="custom_banners">
+    <div class="container">
+      <div class="banner_slider_wrap">
+        <div class="banner_slider">
+          <div class="banner">
+            <figure>
+              <img src="catalog/view/theme/Gobazar/image/images/banner.jpg" alt="">
+            </figure>
+            <div class="banner_content">
+              <div class="banner_text"><span>Диски колеса на JCB</span>Всего <span>10 750 ₽</span></div>
+            </div>
+          </div>
+          <div class="banner">
+            <figure>
+              <img src="catalog/view/theme/Gobazar/image/images/banner.jpg" alt="">
+            </figure>
+            <div class="banner_content">
+              <div class="banner_text"><span>Диски колеса на JCB Диски колеса на JCB</span>Всего <span>10 750 ₽</span></div>
+            </div>
+          </div>
+          <div class="banner">
+            <figure>
+              <img src="catalog/view/theme/Gobazar/image/images/banner.jpg" alt="">
+            </figure>
+            <div class="banner_content">
+              <div class="banner_text"><span>Диски колеса на JCB</span>Всего <span>10 75000 ₽</span></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="brands">
+    <div class="containr">
+      <div class="section_title">Популярные бренды</div>
+      <div class="brands_list">
+        <div class="brand_item">
+          <figure>
+            <img src="catalog/view/theme/Gobazar/image/images/client1.jpg" alt="">
+          </figure>
+        </div>
+        <div class="brand_item">
+          <figure>
+            <img src="catalog/view/theme/Gobazar/image/images/client2.jpg" alt="">
+          </figure>
+        </div>
+        <div class="brand_item">
+          <figure>
+            <img src="catalog/view/theme/Gobazar/image/images/client3.jpg" alt="">
+          </figure>
+        </div>
+        <div class="brand_item">
+          <figure>
+            <img src="catalog/view/theme/Gobazar/image/images/client4.jpg" alt="">
+          </figure>
+        </div>
+        <div class="brand_item">
+          <figure>
+            <img src="catalog/view/theme/Gobazar/image/images/client5.jpg" alt="">
+          </figure>
+        </div>
+        <div class="brand_item">
+          <figure>
+            <img src="catalog/view/theme/Gobazar/image/images/client6.jpg" alt="">
+          </figure>
+        </div>
+        <div class="brand_item">
+          <figure>
+            <img src="catalog/view/theme/Gobazar/image/images/client7.jpg" alt="">
+          </figure>
+        </div>
+        <div class="brand_item">
+          <figure>
+            <img src="catalog/view/theme/Gobazar/image/images/client8.jpg" alt="">
+          </figure>
+        </div>
+        <div class="brand_item">
+          <figure>
+            <img src="catalog/view/theme/Gobazar/image/images/client9.jpg" alt="">
+          </figure>
+        </div>
+        <div class="brand_item">
+          <figure>
+            <img src="catalog/view/theme/Gobazar/image/images/client10.jpg" alt="">
+          </figure>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+
 </body>
 </html>
